86f19f11aa16c98d466b232b576c9fe4c1ff4b8f
liyao <li_yao2000@126.com>
2022-07-05 22:04:35 +0800

    消息订阅pubsub

---
+++ 02_src_pubsub/components/School.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>{{ name }}</h3>
+    <h3>{{ address }}</h3>
+  </div>
+</template>
+
+<script>
+import PubSub  from "pubsub-js";
+export default {
+  name: "SchoolInfo",
+  data() {
+    return {
+      name: "SchoolA",
+      address: "beijing ",
+    };
+  },
+
+  mounted() {
+    this.pubId = PubSub.subscribe('hello',(msg,data) => { 
+      console.log('student收到：',msg,data)
+     })
+  },
+  beforeDestroy(){
+    PubSub.unsubscribe(this.pubId)
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ 02_src_pubsub/components/Student.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>{{ name }}</h3>
+    <h3>{{ age }}</h3>
+    <button @click="sendStudentName">把学生名传给school</button>
+  </div>
+</template>
+
+<script>
+import PubSub from "pubsub-js";
+export default {
+  name: "StudentInfo",
+  data() {
+    return {
+      name: "tom",
+      age: 29,
+    };
+  },
+  methods: {
+    sendStudentName() {
+     PubSub.publish('hello',this.name);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
