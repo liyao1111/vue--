56e9f67de09a734e7c4ed94e5dc107b525216e98
liyao <li_yao2000@126.com>
2022-07-06 14:51:32 +0800

    readme

---
+++ 06_src_默认插槽/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app" class="container">
+    <Category :listData="foods" title="美食">
+      <img src="./assets/logo.png" alt="" />
+    </Category>
+    <Category title="游戏">
+      <ul>
+        <li v-for="(game, index) in games" :key="index">
+          {{ game }}
+        </li>
+      </ul>
+    </Category>
+    <Category title="电影">
+      <video class="film" controls :src="films"></video>
+    </Category>
+  </div>
+</template>
+
+<script>
+import Category from "./components/Category.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      foods: ["apple", "banna", "orange", "peach"],
+      games: ["lol", "dota", "cod", "dnf"],
+      films: '//player.bilibili.com/player.html?aid=804122638&bvid=BV1Zy4y1K7SH&cid=373348512&page=102',
+    };
+  },
+  components: { Category },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ 06_src_默认插槽/components/Category.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="Category">
+    <h3>{{title}}</h3>
+    <!-- 定义一个挖槽(挖个坑，等待组件使用者填充) -->
+    <slot>我是默认值，使用者未传递具体结构时我会出现</slot>
+    <ul>
+      <li v-for="(item, index) of listData" :Key="index">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  data() {
+    return {};
+  },
+  props: ['title'],
+};
+</script>
+
+<style>
+.Category {
+  width: 200px;
+  height: 400px;
+  background-color: skyblue;
+  border: 1px blue solid;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+.film{
+  width: 100%;
+}
+</style>
