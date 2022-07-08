2540282f72f36bf69156d28c3a76d27b6dbf2459
liyao <li_yao2000@126.com>
2022-07-08 23:47:01 +0800

    基本路由

---
+++ 15_src_基本路由/pages/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>this is about</div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  mounted() {
+    console.log("about挂载");
+  },
+  beforeDestroy() {
+    console.log("about即将被销毁");
+  },
+  destroyed(){
+    console.log('about销毁')
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ 15_src_基本路由/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>this is home</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  mounted() {
+    console.log("home挂载");
+  },
+  beforeDestroy() {
+    console.log("home即将被销毁");
+  },
+  destroyed() {
+    console.log("home销毁");
+  },
+};
+</script>
+
+<style>
+</style>
