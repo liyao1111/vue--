95cf56402ee7dbd2ded49e885f4432b40f3fa143
liyao <li_yao2000@126.com>
2022-07-08 23:43:26 +0800

    使用计算属性的vuex的求和案例

---
+++ 10_src_求和案例_vuex_计算属性/components/Count.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h1>当前求和为:{{ sum }}</h1>
+    <h1>大和为:{{ bigSum }}</h1>
+    <h2>我在{{ school }}学习，学习{{ subject }}</h2>
+
+    <select v-model="number">
+      <option :value="1">1</option>
+      <option :value="2">2</option>
+      <option :value="3">3</option>
+    </select>
+    <button @click="decrease">-</button>
+    <button @click="increase">+</button>
+    <button @click="oddIncrease">当前求和为奇数再加</button>
+    <button @click="delayIncrease">等一等再加</button>
+  </div>
+</template>
+
+<script>
+import { mapState,mapGetters } from "vuex";
+export default {
+  name: "Count",
+  data() {
+    return {
+      number: 1,
+    };
+  },
+  methods: {
+    decrease() {
+      this.$store.dispatch("dec", this.number);
+    },
+    increase() {
+      this.$store.dispatch("inc", this.number);
+    },
+    oddIncrease() {
+      this.$store.dispatch("odd", this.number);
+    },
+    delayIncrease() {
+      this.$store.dispatch("delay", this.number);
+    },
+  },
+  computed: {
+    靠程序员自己写计算属性
+    sum() {
+      return this.$store.state.sum;
+    },
+    school() {
+      return this.$store.state.school;
+    },
+    subject() {
+      return this.$store.state.subject;
+    },
+    bigSum() {
+      return this.$store.getters.bigSum;
+    },
+
+  },
+};
+</script>
+
+<style>
+button {
+  margin-left: 10px;
+}
+</style>
