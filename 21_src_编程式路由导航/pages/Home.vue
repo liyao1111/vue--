744fff5a972182f32010af5dd0cca7c0e230dc5d
liyao <li_yao2000@126.com>
2022-07-09 17:52:48 +0800

    编程式路由导航

---
+++ 21_src_编程式路由导航/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>this is home</h2>
+    <div>
+       <!--多级路由路径设置  -->
+      <router-link to="/home/news" class="router">news</router-link>
+      <router-link to="/home/message" class="router">message</router-link>
+    </div>
+    <div>
+      <router-view></router-view>
+    </div>
+    
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+};
+</script>
+
+<style scoped>
+.router{
+  width: 65px;
+  height: 50px;
+  border:1px black solid;
+}
+</style>
