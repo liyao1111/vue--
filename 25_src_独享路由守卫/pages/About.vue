273714ec7d021f2dd07ccc2d5344616573438b18
liyao <li_yao2000@126.com>
2022-07-10 00:31:54 +0800

    独享路由守卫

---
+++ 25_src_独享路由守卫/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>this is about</div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  activated() {
+    console.log("##about激活", this.router);
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 25_src_独享路由守卫/router/index.js
@@ -0,0 +1,68 @@
+// 该文件专门创建整个应用的路由器
+import VueRouter from "vue-router";
+import About from '../pages/About'
+import Home from '../pages/Home'
+import News from '../pages/News'
+import Message from '../pages/Message'
+import Detail from '../pages/Detail'
+const router = new VueRouter({
+    routes: [
+        // 一级路由
+        {
+            path: '/about',
+            component: About,
+            meta: { title: '关于' }
+        },
+        {
+            path: '/home',
+            component: Home,
+            meta: { title: '主页' },
+            // 嵌套路由
+            children: [
+                {
+                    // 不要加 /
+                    name: 'xinwen',
+                    path: 'news',
+                    component: News,
+                    meta: { isAuth: true, title: '新闻' },
+                    beforeEnter: (to, from, next) => {
+                        console.log('独享守卫', to, from)
+                        if (to.meta.isAuth) {
+                            if (localStorage.getItem('name') === 'jack') {
+                                next()
+                            } else {
+                                alert('名字不匹配')
+                            }
+                        } else {
+                            next()
+                        }
+                    }
+                },
+                {
+                    path: 'message',
+                    component: Message,
+                    meta: { isAuth: true, title: '消息' },
+                    children: [
+                        {
+                            name: 'xiangqing',
+                            path: 'detail/:id/:title',
+                            component: Detail,
+                            meta: { title: '消息详情' },
+                            // props第一种写法：值为对象，该对象中所有key-value都会以props形式传给Detail组件
+                            // props:{a:1,b:'hello'}
+                            // props第二种写法：值为布尔值，若为真，则将该路由组件收到的所有params参数 以props形式传给Detail组件
+                            // props:true
+                            // props第三种写法：值为函数
+                            props(route) {
+                                return { id: route.params.id, title: route.params.title }
+                            }
+                        }
+                    ]
+
+                }
+            ]
+        }
+    ]
+})
+
+export default router
