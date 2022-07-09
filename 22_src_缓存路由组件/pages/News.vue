ad54d4700c97645c84d579aa62e787da60f4fac1
liyao <li_yao2000@126.com>
2022-07-09 18:21:09 +0800

    缓存路由组件

---
+++ 22_src_缓存路由组件/pages/News.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>news</h3>
+    <ul>
+      <li><a href="#">news001</a><input type="text" /></li>
+      <li><a href="#">news002</a><input type="text" /></li>
+      <li><a href="#">news003</a><input type="text" /></li>
+      <li><a href="#">news004</a><input type="text" /></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "News",
+};
+</script>
+
+<style>
+</style>
