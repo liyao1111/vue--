e22eebb71be2ede8f1f0381968969a93ccd9281a
liyao <li_yao2000@126.com>
2022-07-10 08:23:28 +0800

    组件路由守卫

---
+++ 26_src_组件路由守卫/pages/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>this is about</div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  // 通过路由规则进入该组件时调用
+  beforeRouteEnter(to, from, next) {
+    console.log("@@@@@进入about组件", from, to);
+    next()
+  },
+  // 通过路由规则离开该组件时调用
+  beforeRouteLeave(to, from,next) {
+    console.log("@@@@离开about", from, to);
+    next()
+    
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 26_src_组件路由守卫/pages/News.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h3>news</h3>
+    <ul>
+      <li :style="{ opacity }">news组件</li>
+      <li><a href="#">news001</a><input type="text" /></li>
+      <li><a href="#">news002</a><input type="text" /></li>
+      <li><a href="#">news003</a><input type="text" /></li>
+      <li><a href="#">news004</a><input type="text" /></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "News",
+  data() {
+    return {
+      opacity: 0.1,
+    };
+  },
+  // 路由组件被激活时触发
+  activated() {
+    console.log("news激活");
+    this.timer = setInterval(() => {
+      this.opacity -= 0.1;
+      if (this.opacity <= 0) this.opacity = 1;
+    }, 1000);
+  },
+  // 路由组件失活时触发
+  deactivated() {
+    console.log("news失活");
+    clearInterval(this.timer);
+  },
+};
+</script>
+
+<style>
+</style>
